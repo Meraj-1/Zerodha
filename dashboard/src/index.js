34cc088997ad1878a9adb4c8345ecd5fa3bcfed3
Meraj-1 <belalraza158@gmail.com>
2026-01-08 00:09:32 +0530

    fix build

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 import Home from "./components/Home";
 import Login from "./auth/Login";
@@ -11,7 +10,6 @@ import LoginSignupPage from "./auth/AuthPage.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -20,6 +18,5 @@ root.render(
           <Route path="/register" element={<Register/>}/>
         </Routes>
       </BrowserRouter>
-    </ThemeProvider>
   </React.StrictMode>
 );
